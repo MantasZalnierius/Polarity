974abc4ad7679991be2c10dd2e52d68d3222651b
FilipRusiecki <57904846+FilipRusiecki@users.noreply.github.com>
2022-01-29 21:31:47 +0000

    3 Enemies done fully

---
+++ Polraity/Assets/CircleRangeHunter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRangeHunter : MonoBehaviour
+{
+
+    public HunterScript hunter;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hunter.hurtPlayer = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hunter.hurtPlayer = false;
+        }
+    }
+}
